0ef05734652a6a870a891323f575ec6d320aea53
Ryan Burst <rburst@mcdpartners.com>
2017-06-06 09:46:32 -0400

    Adding concat and babeled scripts to build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var watch = require('gulp-watch');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
 
 // Variables
 var input = './src/scss/**/*.scss';
@@ -20,6 +22,15 @@ gulp.task('default', function () {
     .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(autoprefixer('last 2 versions'))
     .pipe(gulp.dest(output));
+});
+
+gulp.task('scripts', function () {
+  return gulp.src('./src/scripts/**/*.js')
+        .pipe(concat('panelz.js'))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./read/js'));
 });
 
 gulp.task('watch', function () {
