7976e38c3ec2737f7f955c42b61f55c824f2fc23
Ryan Burst <rburst@mcdpartners.com>
2017-06-18 08:12:26 -0400

    debug

--- dist/js/panelz.js
+++ dist/js/panelz.js
@@ -1143,16 +1143,16 @@ var ViewPort = function (_EventClass6) {
 
         _this6.interactable = new Hammer.Manager(_this6.$element.find('.viewport__interactable')[0]);
 
-        var pan = new Hammer.Pan({ threshold: 20, enable: _this6.canRecognizePan.bind(_this6) });
+        //var pan = new Hammer.Pan({threshold: 20, enable: this.canRecognizePan.bind(this)});
         var pinch = new Hammer.Pinch({ threshold: 0, enable: true });
-        var singletap = new Hammer.Tap({ threshold: 2, posThreshold: 150 });
-        var doubletap = new Hammer.Tap({ event: 'doubletap', taps: 2 });
-        var swipe = new Hammer.Swipe({ enable: _this6.canRecognizeSwipe.bind(_this6) });
+        //var singletap = new Hammer.Tap({threshold: 2, posThreshold: 150});
+        //var doubletap = new Hammer.Tap({event: 'doubletap', taps: 2 });
+        //var swipe = new Hammer.Swipe({enable: this.canRecognizeSwipe.bind(this)});
 
-        _this6.interactable.add([pan, singletap, doubletap, swipe, pinch]);
+        _this6.interactable.add([/*pan,singletap,doubletap,swipe,*/pinch]);
 
-        singletap.requireFailure(doubletap);
-        pan.requireFailure(pinch);
+        //singletap.requireFailure(doubletap);
+        //pan.requireFailure(pinch);
 
         _this6.interactable.get('pinch').set({ enable: true });
 
--- src/scripts/ViewPort.js
+++ src/scripts/ViewPort.js
@@ -19,16 +19,16 @@ class ViewPort extends EventClass {
 
         this.interactable = new Hammer.Manager(this.$element.find('.viewport__interactable')[0]);
 
-        var pan = new Hammer.Pan({threshold: 20, enable: this.canRecognizePan.bind(this)});
+        //var pan = new Hammer.Pan({threshold: 20, enable: this.canRecognizePan.bind(this)});
         var pinch = new Hammer.Pinch({ threshold: 0, enable: true });
-        var singletap = new Hammer.Tap({threshold: 2, posThreshold: 150});
-        var doubletap = new Hammer.Tap({event: 'doubletap', taps: 2 });
-        var swipe = new Hammer.Swipe({enable: this.canRecognizeSwipe.bind(this)});
+        //var singletap = new Hammer.Tap({threshold: 2, posThreshold: 150});
+        //var doubletap = new Hammer.Tap({event: 'doubletap', taps: 2 });
+        //var swipe = new Hammer.Swipe({enable: this.canRecognizeSwipe.bind(this)});
 
-        this.interactable.add([pan,singletap,doubletap,swipe,pinch]);
+        this.interactable.add([/*pan,singletap,doubletap,swipe,*/pinch]);
 
-        singletap.requireFailure(doubletap);
-        pan.requireFailure(pinch);
+        //singletap.requireFailure(doubletap);
+        //pan.requireFailure(pinch);
 
         this.interactable.get('pinch').set({ enable: true });
 
